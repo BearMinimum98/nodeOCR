1f48754677df31426e61700ff64c969a341b682c
KevZho <KevZho141@gmail.com>
2013-11-16 13:50:03 -0800

    more temp files for multiple requests at same time

--- nodeOCR/ocr.js
+++ nodeOCR/ocr.js
@@ -1,7 +1,6 @@
-var nodecr = require('nodecr');
+var nodecr = require("nodecr");
 var http = require("http");
-var url = require('url');
-var fs = require('fs');
+var fs = require("fs");
 
 function start() {
 	function onRequest(request, response) {
@@ -10,44 +9,61 @@ function start() {
 			body += chunk;
 		});
 		request.on("end", function() {
-			console.log("\n" + (new Date()) + ": Request received.");
+			console.log((new Date()) + ": Request received.\n");
+			
 			if (body != "") {
 				body = body.replace(/^data:image\/jpeg;base64,/,"");
-				fs.writeFile("temp/temp.jpg", body, 'base64', function(err) {
+				var random = Math.floor(Math.random() * 100000);
+				
+				fs.writeFile("temp/temp" + random + ".jpg", body, 'base64', function(err) {
 					if (err) {
-						console.log((new Date()) + ": " + err);
+						console.log("\n" + (new Date()) + ": " + err);
+					} else {
+						console.log("\n" + (new Date()) + ": temp%d.jpg created", random);
 					}
 				});
+				
+				nodecr.process("temp/temp" + random + ".jpg", function(err, text) {
+					if (err) {
+						console.log("Tesseract error!");
+						
+						response.writeHead(500, {"Content-Type": "text/plain", "Access-Control-Allow-Origin": "*"});
+						response.write("Tesseract error");
+						response.end();
+						
+						fs.unlink("temp/temp" + random + ".jpg", function(err) {
+							if (err) {
+								console.log((new Date()) + ": Couldn't delete temp%d.jpg!", random);
+							} else {
+								console.log((new Date()) + ": temp%d.jpg deleted", random);
+							}
+						});
+					} else {
+						console.log("\nSuccess: %s", text);
+						
+						response.writeHead(200, {"Content-Type": "text/plain", "Access-Control-Allow-Origin": "*"});
+						response.write(text.substring(0, text.length));
+						response.end();
+						
+						fs.unlink("temp/temp" + random + ".jpg", function(err) {
+							if (err) {
+								console.log((new Date()) + ": Couldn't delete temp%d.jpg!", random);
+							} else {
+								console.log((new Date()) + ": temp%d.jpg deleted", random);
+							}
+						});
+					}
+				});
+			} else {
+				console.log((new Date()) + ": No body found!");
+				
+				response.writeHead(500, {"Content-Type": "text/plain", "Access-Control-Allow-Origin": "*"});
+				response.write("No body found!");
+				response.end();
 			}
-			nodecr.process("temp/temp.jpg", function(err, text) {
-				if (err) {
-					console.log("Tesseract error!");
-					response.writeHead(500, {"Content-Type": "text/plain", "Access-Control-Allow-Origin": "*"});
-					response.write("Tesseract error");
-					response.end();
-					fs.unlink("temp/temp.jpg", function(err) {
-						if (err) {
-							console.log((new Date()) + ": Couldn't delete temp.jpg!");
-						} else {
-							console.log((new Date()) + ": temp.jpg deleted");
-						}
-					});
-				} else {
-					console.log("Success: %s", text);
-					response.writeHead(200, {"Content-Type": "text/plain", "Access-Control-Allow-Origin": "*"});
-					response.write(text.substring(0, text.length));
-					response.end();
-					fs.unlink("temp/temp.jpg", function(err) {
-						if (err) {
-							console.log((new Date()) + ": Couldn't delete temp.jpg!");
-						} else {
-							console.log((new Date()) + ": temp.jpg deleted");
-						}
-					});
-				}
-			});
 		});
 	}
+	
 	http.createServer(onRequest).listen(8888);
 	console.log((new Date()) + ": Server has started.");
 }
