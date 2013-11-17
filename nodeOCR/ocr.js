9d69d146c6ce3c69afdcdef96062cf1eab334385
KevZho <KevZho141@gmail.com>
2013-11-17 13:26:15 -0800

    rewrite ocr.js

--- nodeOCR/ocr.js
+++ nodeOCR/ocr.js
@@ -1,20 +1,16 @@
 var nodecr = require("nodecr");
 var http = require("http");
 var fs = require("fs");
+var WebSocketServer = require("ws").Server;
 
 function start() {
-	function onRequest(request, response) {
-		var body = "";
-		request.on("data", function (chunk) {
-			body += chunk;
-		});
-		request.on("end", function() {
-			console.log((new Date()) + ": Request received.\n");
-			
-			if (body != "") {
-				body = body.replace(/^data:image\/jpeg;base64,/,"");
+	var ws = new WebSocketServer({port: 8888});
+	wss.on("connection", function (ws) {
+		ws.on("message", function (message) {
+			console.log((new Date()) + ": Message received.\n");
+			if (message != "") {
+				var body = message.replace(/^data:image\/jpeg;base64,/,"");
 				var random = Math.floor(Math.random() * 100000);
-				
 				fs.writeFile("temp/temp" + random + ".jpg", body, 'base64', function(err) {
 					if (err) {
 						console.log("\n" + (new Date()) + ": " + err);
@@ -22,15 +18,10 @@ function start() {
 						console.log("\n" + (new Date()) + ": temp%d.jpg created", random);
 					}
 				});
-				
 				nodecr.process("temp/temp" + random + ".jpg", function(err, text) {
 					if (err) {
 						console.log("Tesseract error!");
-						
-						response.writeHead(500, {"Content-Type": "text/plain", "Access-Control-Allow-Origin": "*"});
-						response.write("Tesseract error");
-						response.end();
-						
+						ws.send("Tesseract error");
 						fs.unlink("temp/temp" + random + ".jpg", function(err) {
 							if (err) {
 								console.log((new Date()) + ": Couldn't delete temp%d.jpg!", random);
@@ -40,11 +31,7 @@ function start() {
 						});
 					} else {
 						console.log("\nSuccess: %s", text);
-						
-						response.writeHead(200, {"Content-Type": "text/plain", "Access-Control-Allow-Origin": "*"});
-						response.write(text.substring(0, text.length));
-						response.end();
-						
+						ws.send(text);
 						fs.unlink("temp/temp" + random + ".jpg", function(err) {
 							if (err) {
 								console.log((new Date()) + ": Couldn't delete temp%d.jpg!", random);
@@ -56,15 +43,10 @@ function start() {
 				});
 			} else {
 				console.log((new Date()) + ": No body found!");
-				
-				response.writeHead(500, {"Content-Type": "text/plain", "Access-Control-Allow-Origin": "*"});
-				response.write("No body found!");
-				response.end();
+				ws.send("No body found!");
 			}
 		});
-	}
-	
-	http.createServer(onRequest).listen(8888);
+	});
 	console.log((new Date()) + ": Server has started.");
 }
 
